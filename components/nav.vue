af045ada8c5892583dad753efa26a66894b83e41
Александр Голубов <Alex@alex.local>
2018-03-17 12:37:00 +0500

    comment extra nav

--- components/nav.vue
+++ components/nav.vue
@@ -3,8 +3,10 @@
     <nav class="f7 fw3 ttu tracked flex flex-column flex-row-m flex-row-l justify-between">
       <div class="flex flex-column flex-row-m flex-row-l">
         <nuxt-link to="/" class="link dim white dib mr3 pv2" title="Countries">Countries</nuxt-link>
+        <!--
         <nuxt-link to="/location" class="link dim white dib mr3 pv2" title="My Location">My Location</nuxt-link>
         <nuxt-link to="/country" class="link dim white dib mr3 pv2" title="Country">Country</nuxt-link>
+        -->
       </div>
       <nuxt-link to="/about" class="link dim white dib pv1" title="About">About</nuxt-link>
     </nav>
